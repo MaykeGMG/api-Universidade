3261c6256bb4c81768a9719e122bbf71a38285ae
MaykeGMG <maykegmg15@gmail.com>
2023-09-26 12:02:23 -0300

    ..

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -11,70 +11,23 @@ namespace apiUniversidade.Controllers
     [Route("[controller]")]
     public class CursoController : ControllerBase
     {
+        private readonly Ilogger<CursoController> _Logger;
 
-        [HttpGet(Name = "cursos")]
+        private readonly apiUniversidade _context;
 
-        public List<Curso> GetCursos()
+        public CursoController(ILogger<CursoController> Logger, apiUniversidadeContext context){
+            _Logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public ActionResult<Enumerable<Curso>> Get()
         {
-
-            List<Aluno> alunos = new List<Aluno>();
-            List<Disciplina> disciplinas = new List<Disciplina>();
-            List<Curso> cursos = new List<Curso>();
-
-            disciplinas.Add(new Disciplina{
-                Nome = "Português",
-                CargaHoraria = 80,
-                Semestre = 4
-            });
-
-            disciplinas.Add(new Disciplina{
-                Nome = "Matemática",
-                CargaHoraria = 80,
-                Semestre = 4
-            });
-
-            disciplinas.Add(new Disciplina{
-                Nome = "História",
-                CargaHoraria = 80,
-                Semestre = 4
-            });
-
-            cursos.Add(new Curso{
-                Nome = "Linguagem",
-                Area = "Letras",
-                Duracao = 4,
-                Disciplinas = disciplinas,
-                Alunos = alunos
-            });
-
-            cursos.Add(new Curso{
-                Nome = "Calcúlos",
-                Area = "Matemática",
-                Duracao = 4,
-                Disciplinas = disciplinas,
-                Alunos = alunos
-            });
-
-            cursos.Add(new Curso{
-                Nome = "História",
-                Area = "História",
-                Duracao = 4,
-                Disciplinas = disciplinas,
-                Alunos = alunos
-            });
-
-            alunos.Add(new Aluno {
-                Nome = "Jardson",
-                dataNascimento = DateTime.Now,
-                CPF = "444-444"
-            });
-
-            alunos.Add(new Aluno {
-                Nome = "Maria",
-                dataNascimento = DateTime.Now,
-                CPF = "444-455"
-            });
-
+            var cursos = _context.Curso.ToList();
+            if(cursos is null)
+                return NotFound();
+                
             return cursos;
         }
     }
